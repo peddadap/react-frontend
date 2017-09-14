584dc64ca97c1e0a0dd3e72f1bb426b951cb304a
Sachin Chouk <sachinc@thedigitalgroup.com>
2017-09-14 21:59:49 +0530

    Commiting fetch for models

--- src/containers/Edit.js
+++ src/containers/Edit.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { PageHeader, ListGroup,ListGroupItem,Button,Nav,NavItem,Navbar,Tabs,Tab} from "react-bootstrap";
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import "./Home.css";
-import jsonData from "../data.json";
+//import jsonData from "../data.json";
 import Config from "./ux.json";
 
 function onAfterSaveCell(row, cellName, cellValue) {
@@ -25,13 +25,46 @@ const cellEditProp = {
 
 
 export default class Edit extends Component {
-  render(tickets) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      jsonData: []
+    };
+  }
+      
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+      
+    try {
+      fetch('/jsonData')
+        .then(res => res.json())
+        .then(jsonData => { console.log('This is my front end log of Termination Object:'); console.log(jsonData);this.setState({ jsonData })} );
+    } catch (e) {
+      alert(e);
+    }
+    this.setState({ isLoading: false });
+  }
+     
+  render(jsonData) {
     return (
 		  <BootstrapTable data={jsonData} cellEdit={ cellEditProp } pagination >
         <TableHeaderColumn isKey dataField='id'>ID</TableHeaderColumn>
-        {
-          Object.keys(Config.oi).map((k, index) => <TableHeaderColumn width = {"150"} dataField={k}>{k}</TableHeaderColumn>)
-        }
+        <TableHeaderColumn dataField='AccountNumber'>AccountNumber</TableHeaderColumn>
+        <TableHeaderColumn dataField='Name1'>Name1</TableHeaderColumn>
+        <TableHeaderColumn dataField='Name2'>Name2</TableHeaderColumn>
+        <TableHeaderColumn dataField='TaxID'>TaxID</TableHeaderColumn>
+        <TableHeaderColumn dataField='TerminationDate'>TerminationDate</TableHeaderColumn>
+        <TableHeaderColumn dataField='SharesTerminated'>SharesTerminated</TableHeaderColumn>
+        <TableHeaderColumn dataField='Disposition'>Disposition</TableHeaderColumn>
+        <TableHeaderColumn dataField='GlobalID'>GlobalID</TableHeaderColumn>
+        <TableHeaderColumn dataField='createdAt'>createdAt</TableHeaderColumn>
+        <TableHeaderColumn dataField='updatedAt'>updatedAt</TableHeaderColumn>
+{/*         {
+          Object.keys(Config.terminations).map((k, index) => <TableHeaderColumn width = {"150"} dataField="AccountNumber">{k}</TableHeaderColumn>)
+        } */}
   	  </BootstrapTable>
     );
   }
